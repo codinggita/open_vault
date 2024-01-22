6d5d5d3893ae4c00b7675fb387f91ecafe72aa9f
shubhansu-kr <shubhansu2021@gmail.com>
2024-01-22 10:04:20 +0530

    Created Token Authentication Middleware

---
+++ Server/middlewares/authenticate.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+const { StatusCodes } = require('http-status-codes');
+
+const authenticate = async (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        return res.status(StatusCodes.MISDIRECTED_REQUEST).send('Enter a valid authorization token');
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        const payload = jwt.verify(token, process.env.SECRET_KEY);
+        req.user = { role: payload.role, id: payload.id };
+        next();
+    } catch (err) {
+        console.log('Error verifying the access token', err);
+        res.status(StatusCodes.MISDIRECTED_REQUEST).send('You are not authorized to access this route');
+    }
+}
+
+module.exports = authenticate;
