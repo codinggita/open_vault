562266315a413d31a8341981a57ee9dc086548fb
shubhansu-kr <shubhansu2021@gmail.com>
2024-01-22 11:35:14 +0530

    Created Route for Drop

---
+++ Server/controllers/dropController.js
@@ -0,0 +1,15 @@
+const { StatusCodes } = require('http-status-codes');
+
+const visitDrop = async () => {
+    // 
+}
+
+const createDrop = async () => {
+    // 
+}
+
+const openDrop = async () => {
+    // 
+}
+
+module.exports = { visitDrop, createDrop, openDrop };
