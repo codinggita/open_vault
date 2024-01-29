deb4780db2422a415a6a21387e3e9b3df374a74e
shubhansu-kr <shubhansu2021@gmail.com>
2024-01-29 12:30:17 +0530

    Completed Frontend Setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import openVaultLogo from './assets/openvault_logo.svg'
 import './App.css'
 
 function App() {
@@ -9,25 +9,19 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://openvault.onrender.com/" target="_blank">
+          <img src={openVaultLogo} className="logo react" alt="openVault logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      {/* <h1>Open Vault</h1> */}
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Encrypt Files | Share Private Data
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <p className="read-the-docs">
+        Get Started (click here)
+      </p> */}
     </>
   )
 }
