4ea0f4d5ca93cda104bc33289d8034243bc63ca9
shubhansu-kr <shubhansu2021@gmail.com>
2024-01-29 16:12:59 +0530

    Routes configured

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,28 +1,31 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
-import openVaultLogo from './assets/openvault_logo.svg'
+import Home from './components/Home'
+import Users from './components/Users'
+import About from './components/About'
+import Nav from './components/Nav'
+
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://openvault.onrender.com/" target="_blank">
-          <img src={openVaultLogo} className="logo react" alt="openVault logo" />
-        </a>
-      </div>
-      {/* <h1>Open Vault</h1> */}
-      <div className="card">
-        <p>
-          Encrypt Files | Share Private Data
-        </p>
-      </div>
-      {/* <p className="read-the-docs">
-        Get Started (click here)
-      </p> */}
-    </>
+
+    <Router>
+      
+      {/* <p className="read-the-docs"> Get Started (click here) </p> */}
+      <Nav></Nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/users" element={<Users />} />
+      </Routes>
+
+    </Router>
+
   )
 }
 
