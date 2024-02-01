379d294e5e6fbf6536c960da3dc324d258c5f140
shubhansu-kr <shubhansu2021@gmail.com>
2024-02-01 11:19:04 +0530

    Added Footer

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 
-function Footer() {
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const defaultTheme = createTheme({
+  palette: {
+    mode: "dark",
+  }
+});
+
+
+const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
-  )
-}
+    <ThemeProvider theme={defaultTheme}>
 
-export default Footer
+      <Paper
+        component="footer"
+        elevation={2} // You can adjust the elevation if you want a shadow
+        style={{
+          padding: '20px',
+          marginTop: '20px',
+          textAlign: 'center',
+          background: '#242424'
+        }}
+      >
+        <Typography variant="body2" color="textSecondary">
+          © {new Date().getFullYear()} openVault. All rights reserved.
+        </Typography>
+      </Paper>
+    </ThemeProvider>
+  );
+};
+
+export default Footer;
