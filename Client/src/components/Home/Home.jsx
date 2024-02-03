d3e3139373c5d192868822f752e3229bd64fe22e
shubhansu-kr <shubhansu2021@gmail.com>
2024-02-03 15:51:32 +0530

    Added Feature Section

--- Client/src/components/Home/Home.jsx
+++ Client/src/components/Home/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 import Hero from './Hero'
 import Stat from './Stat'
+import About from './About'
+import Encrypt from '../Feature/Encrypt'
 
 function Home() {
   return (
     <>
       <Hero />
       <Stat />
-
+      <About />
+      <Encrypt />
     </>
   )
 }
