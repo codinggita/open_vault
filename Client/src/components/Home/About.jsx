70317f3da4774b63a262fb639d2f67a8b25b9b07
shubhansu-kr <shubhansu2021@gmail.com>
2024-02-05 14:41:24 +0530

    Removed redundant import statements

--- Client/src/components/Home/About.jsx
+++ Client/src/components/Home/About.jsx
@@ -1,16 +1,10 @@
 import * as React from 'react';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import Logo from '../accessibility/Logo';
-import { useNavigate } from 'react-router-dom';
 
 import svg1 from './../../assets/aboutSVG1.svg';
 import svg2 from './../../assets/aboutSVG2.svg';
--- Client/src/components/Home/Stat.jsx
+++ Client/src/components/Home/Stat.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Container from '@mui/material/Container';
-import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import { Box, Grid } from '@mui/material';
 
