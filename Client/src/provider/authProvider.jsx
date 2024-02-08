1bb2b210c3759396ebba0cfce9035a5d46000fcb
Shubhansu kumar <shubhansu2021@gmail.com>
2024-02-08 16:21:20 +0530

    BugFix: AuthProvider

--- Client/src/provider/authProvider.jsx
+++ Client/src/provider/authProvider.jsx
@@ -4,30 +4,34 @@ import { createContext, useContext, useEffect, useMemo, useState } from "react";
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-	const [token, setToken] = useState(localStorage.getItem("token"));
+	// State to hold the authentication token
+	const [token, setToken_] = useState(localStorage.getItem("token"));
 
-	const setToken_ = (newToken) => {
-		setToken(newToken);
+	// Function to set the authentication token
+	const setToken = (newToken) => {
+		setToken_(newToken);
 	};
 
 	useEffect(() => {
 		if (token) {
-			axios.defaults.headers.common["authorization"] = "Bearer " + token;
+			axios.defaults.headers.common["Authorization"] = "Bearer " + token;
 			localStorage.setItem("token", token);
 		} else {
-			delete axios.defaults.headers.cpmmon["authorization"];
+			delete axios.defaults.headers.common["Authorization"];
 			localStorage.removeItem("token");
 		}
 	}, [token]);
 
+	// Memoized value of the authentication context
 	const contextValue = useMemo(
 		() => ({
 			token,
-			setToken_,
+			setToken,
 		}),
 		[token]
 	);
 
+	// Provide the authentication context to the children components
 	return (
 		<AuthContext.Provider value={contextValue}>
 			{children}
